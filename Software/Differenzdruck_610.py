05e50e3fa344fd68c0d60de0d8c753dfb4cbefb2
Alex <alexander.sommerfeld@stud.th-rosenheim.de>
2025-06-11 17:25:03 +0200

    Sensoren Software Update

---
+++ Software/Differenzdruck_610.py
@@ -0,0 +1,33 @@
+import smbus2 
+import time
+
+SDP610_ADDR = 0x40
+bus=smbus2.SMBus(1)
+
+def read_sdp610(bus):
+    try:
+        # Lese 2 Bytes vom Sensor
+        data = bus.read_i2c_block_data(SDP610_ADDR, 0xF1, 2)  # 0xF1 = Read Pressure Command
+        raw_pressure = (data[0] << 8) | data[1]
+
+        # Werte sind als signed integer
+        if raw_pressure >= 32768:
+            raw_pressure -= 65536
+
+        # Umrechnung laut Sensirion-Datenblatt (bei SDP610-500Pa)
+        pressure_pa = raw_pressure * 60.0 / 16384.0  # Umrechnung in Pascal
+
+        return pressure_pa
+
+    except Exception as e:
+        return None
+
+def Drucksensor():
+    pressure = read_sdp610(bus)
+    if pressure is not None:
+        time.sleep(1)
+        return pressure
+
+#Fürs Hauptprogramm
+
+Drucksensor()
