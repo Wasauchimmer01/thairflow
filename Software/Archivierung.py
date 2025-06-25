bbe08692639847e443a867cefb331d914333bdf9
Alexander Sommerfeld <alexander.sommerfeld@stud.th-rosenheim.de>
2025-06-25 16:10:32 +0200

    Archivierung added

--- Software/Archivierung.py
+++ Software/Archivierung.py
@@ -1,10 +1,8 @@
 from openpyxl import load_workbook
+from test import write_data_xlsx
 
-workbook=None
 
-def open_book(name):
-    workbook = load_workbook('meinedatei.xlsx')
-
+workbook = load_workbook('Daten.xlsx')
 
 def new_bookpage(name):
     if name not in workbook.sheetnames:
@@ -14,4 +12,8 @@ def switch_active_page(name):
     workbook.active = workbook[name]
 
 def save_data():
-    workbook.save('datensammlung.xlsx')
+    workbook.save('Daten.xlsx')
+
+def write_data(data,page):
+    switch_active_page(page)
+    write_data_xlsx(data)
--- Software/test.py
+++ Software/test.py
@@ -1,26 +1,32 @@
 import csv
 from datetime import datetime
 from time import sleep
+#import Archivierung
 
 measurement_Time = datetime.now().isoformat()
 
 gasdata= measurement_Time,'Gas','OFF',0,'XXX'
 formdata= measurement_Time,'Form','ON',0,'XVX'
+co2data= measurement_Time,'CO2','ON',0,'XZX'
 
-data=[ gasdata,formdata]
+data=[gasdata,formdata,co2data]
 y=1
-while y <1000:  
-    with open('Daten.csv', "r", newline="") as f:
-            lines = list(csv.reader(f)) 
-    while len(lines) < (y*2):
-            lines.append([])
-    x =0
-    while x<2:    
-        lines[(y) - 1+x] = data[x]
-        with open('Daten.csv', "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerows(lines)
-        x=x+1
-    y=y+1
+x=0
 
-sleep(1)
+def write_data_xlsx(data):
+    while y <len(data):  
+        with open('Daten.xlsx', "r", newline="") as f:
+                lines = list(csv.reader(f)) 
+        while len(lines) < (y*2):
+                lines.append([])
+        x =0
+        while x<3:    
+            lines[y+x-(len(data)-2)] = data[x]
+            with open('Daten.xlsx', "w", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerows(lines)
+            x=x+1
+        y=y+len(data)
+    sleep(1)
+
+
