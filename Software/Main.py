a90c99cdefc541747d8b1827313f79a93819af11
Alexander Sommerfeld <alexander.sommerfeld@stud.th-rosenheim.de>
2025-07-02 14:20:13 +0200

    testlauf

--- Software/Main.py
+++ Software/Main.py
@@ -1,30 +1,29 @@
-import Gassensor
-import Differenzdruck_610
-import Differenzdruck_810
-import Formaldehyd
-import Gyroskop_Fenster
-import Gyroskop_Tuer
-import Motionsensor
+import gassensor
+import differenzdruck_610
+import differenzdruck_810
+import formaldehyd
+import gyroskop_Fenster
+import gyroskop_Tuer
+import motionsensor
 import gpiozero
 from datetime import datetime
-import Config
+import config
 import csv
-
-
+from time import sleep
 
 sensor_anzahl=7
 startline=0
 init_completed = 0 #wird 1 wenn das initialisieren abgeschlossen ist
 messdaten_counter =0 #wird bei jeder Messung erhöht
 
-def Initialisieren():
+def initialisieren():
     #Nummer nach Reihenfolge vergeben
-    Config.add_sensor(Gassensor.start_up(1))
-    Config.add_sensor(Formaldehyd.start_up(2))
-    Config.add_sensor(Gyroskop_Fenster.start_up(3))
-    Config.add_sensor(Gyroskop_Tuer.start_up(4))
-    Config.add_sensor(Differenzdruck_610.start_up(5))
-    Config.add_sensor(Differenzdruck_810.start_up(6))
+    # config.add_sensor(gassensor.start_up(1))
+    # config.add_sensor(formaldehyd.start_up(2))
+    # config.add_sensor(gyroskop_Fenster.start_up(3))
+    # config.add_sensor(gyroskop_Tuer.start_up(4))
+    config.add_sensor(differenzdruck_610.start_up(1))
+    # config.add_sensor(differenzdruck_810.start_up(6))
 
     with open('config.csv') as csvdatei:
         csv_reader_object = csv.reader(csvdatei)
@@ -37,52 +36,56 @@ def Initialisieren():
                 #print('Initialisieren complete')
                 return 0
 
-def Messdaten_generieren():
+def messdaten_generieren():
 
-    if Gassensor.check_data==True:
-       gas_data = Gassensor.read_measurement    
+    # if gassensor.check_data==True:
+    #    gas_data = gassensor.read_measurement    
 
-    differenz_data = Differenzdruck_610.read_measurement
+    differenz_data = differenzdruck_610.read_measurement
 
-    formaldehyd_data = Formaldehyd.read_measurement
+    # formaldehyd_data = formaldehyd.read_measurement
 
-    if Motionsensor.monitor_motion==True:
-        motion_data = True
-    else: 
-        motion_data = False
+    # if motionsensor.monitor_motion==True:
+    #     motion_data = True
+    # else: 
+    #     motion_data = False
 
-    gyro_fenster_data = Gyroskop_Fenster.get_sensor_data
+    # gyro_fenster_data = gyroskop_Fenster.get_sensor_data
 
-    gyro_tuer_data = Gyroskop_Tuer.get_sensor_data
+    # gyro_tuer_data = gyroskop_Tuer.get_sensor_data
 
-    measurement_Time = datetime.now().isoformat()
+    # measurement_Time = datetime.now().isoformat()
 
-    gasdata= measurement_Time,'Gas','OFF',1,'XXX'
-    formdata= measurement_Time,'Form','ON',2,'XVX'
-
-    data=[ gasdata,formdata]
+    # data=[measurement_Time,gas_data,formaldehyd_data,differenz_data,motion_data,gyro_fenster_data,gyro_tuer_data]
     
-    with open('Daten.csv', "r", newline="") as f:
-        lines = list(csv.reader(f)) 
-    while len(lines) < (startline+messdaten_counter*sensor_anzahl):
-        lines.append([])
-    for x in data:    
-        lines[(startline+messdaten_counter*sensor_anzahl) - 1+x] = data[x]
-        with open('Daten.csv', "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerows(lines)
+    xlsx_data(differenz_data)
 
+def xlsx_data(data):
+    y=1
+    while y <len(data):  
+        with open('daten.xlsx', "r", newline="") as f:
+                lines = list(csv.reader(f)) 
+        # while len(lines) < (y*2):
+        #         lines.append([])
+        x =0
+        while x<len(data):    
+            lines.append (data[x])
+            with open('daten.xlsx', "w", newline="") as f:
+                writer = csv.writer(f)
+                writer.writerows(lines)
+            x=x+1
+        y=y+len(data)
+    sleep(1)
 
 while True:
     if init_completed!=1:
-        if Initialisieren!=0:
+        if initialisieren!=0:
             print('Initialisieren fehlgeschlagen')
             break
+        else:
+            init_completed=1        
+            print('Initialisieren erfolgreich')
 
-    allData=Messdaten_generieren 
+    messdaten_generieren 
     messdaten_counter=messdaten_counter+1
-
-
-
-
-
+    sleep(10)
--- Software/test.py
+++ Software/test.py
@@ -8,25 +8,28 @@ measurement_Time = datetime.now().isoformat()
 gasdata= measurement_Time,'Gas','OFF',0,'XXX'
 formdata= measurement_Time,'Form','ON',0,'XVX'
 co2data= measurement_Time,'CO2','ON',0,'XZX'
+co2data1= measurement_Time,'CO21','ON',0,'XZX'
+co2data2= measurement_Time,'CO22','ON',0,'XZX'
+co2data3= measurement_Time,'CO23','ON',0,'XZX'
 
-data=[gasdata,formdata,co2data]
-y=1
-x=0
+data = [gasdata,formdata,co2data,co2data1,co2data2,co2data3]
 
-def write_data_xlsx(data):
+
+def xlsx_data():
+    y=1
     while y <len(data):  
-        with open('Daten.xlsx', "r", newline="") as f:
+        with open('daten.xlsx', "r", newline="") as f:
                 lines = list(csv.reader(f)) 
-        while len(lines) < (y*2):
-                lines.append([])
+        # while len(lines) < (y*2):
+        #         lines.append([])
         x =0
-        while x<3:    
-            lines[y+x-(len(data)-2)] = data[x]
-            with open('Daten.xlsx', "w", newline="") as f:
+        while x<len(data):    
+            lines.append (data[x])
+            with open('daten.xlsx', "w", newline="") as f:
                 writer = csv.writer(f)
                 writer.writerows(lines)
             x=x+1
         y=y+len(data)
     sleep(1)
 
-
+xlsx_data()
